a75dc36610c49df4c21a4d92c955c65bd1ab4271
jimmy <jimmy>
2022-11-29 20:34:44 +0700

    commit 2

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,6 +1,4 @@
 docker build -t item-app:v1 . # Melakukan docker build dengan nama image item-app versi v1
 docker image ls -a # Mengeluarkan daftar image
 docker tag item-app:v1 ghcr.io/jimmyready89/item-app:v1 # Rename nama image
-export PASSWORD_GITHUB=<password_Anda> #Setting variable password github
-echo $PASSWORD_GITHUB | docker login ghcr.io -u fikrihelmi17 --password-stdin #Perintah login ke github
 docker push ghcr.io/jimmyready89/item-app:v1 # pull image dari local pc ke github packages
