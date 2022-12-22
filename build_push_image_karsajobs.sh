a7f1df00131faaece371f637c15b494582317735
jimmy ready <jimmyready>
2022-12-22 22:18:42 +0700

    Pull cofing

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,3 @@
+docker build -t ghcr.io/jimmyready89/karsajobs:latest . # Melakukan docker build dengan nama image item-app versi v1
+echo $PASSWORD_GITHUB | docker login ghcr.io -u jimmyready89 --password-stdin #Perintah login ke github
+docker push ghcr.io/jimmyready89/karsajobs:latest # pull image dari local pc ke github packages
